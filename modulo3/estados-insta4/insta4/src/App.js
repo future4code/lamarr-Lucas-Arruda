09601b9e73c4401cdba6b076bd0f674804dc03fe
Lucas <luucasferreiira21@gmail.com>
2022-07-08 00:06:39 -0300

    modificar arquivos

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './components/Post/Post';
-import { MainContainer, GlobalStyle} from './style';
+import { MainContainer, GlobalStyle, Form} from './style';
 
 
 
@@ -8,12 +8,27 @@ import { MainContainer, GlobalStyle} from './style';
 
 function App() {
 
-const listagem = [
+const [listagem, setListagem] = useState ([
   {nomeUsuario: 'j040', fotoUsuario:'https://cdn.dicionariopopular.com/imagens/imagem-de-perfil-gato.jpg', fotoPost: 'https://live.apto.vc/wp-content/uploads/2022/01/setup-gamer.jpg'},
   {nomeUsuario: 'luxcasf', fotoUsuario: 'https://i0.wp.com/emotioncard.com.br/wp-content/uploads/2016/05/perfil-whatsapp.jpg?fit=600%2C600&ssl=1', fotoPost: 'https://i.pinimg.com/originals/db/0b/b7/db0bb7f339bf3aef6641af084c91b3a7.jpg'},
   {nomeUsuario: 'r4fa', fotoUsuario: 'https://cdn.dicionariopopular.com/imagens/fotoengracada-de-perfil-micao-jackson2.jpg', fotoPost: 'https://gtprogamer.com.br/wp-content/uploads/2021/12/Game-Room-Ideas-For-Small-Rooms.jpg'}
 
-]
+])
+
+// eventos
+
+// const handleInputNome = (evento) => {
+//   evento.target.value
+
+// }
+
+// const handleInputPerfil = (evento) => {
+//   evento.target.value
+// }
+
+// const handleInputPostagem = (evento) => {
+//   evento.target.value
+// }
 
 let listagemGeral=listagem.map((e, i)=>{
   return (
@@ -33,6 +48,32 @@ console.log(listagemGeral)
 return(
   <MainContainer>
       <GlobalStyle/>
+
+      {/* A linha de codigo abaixo nao ficou definida, até o momento da entrega do trabalho. */}
+      {/* <Form>
+        <label>Usuário:</label>
+        <input 
+            placeholder='Insira o seu Usuário'
+            onChange={handleInputNome}
+            value
+           />
+        
+        <label>Url de Perfil:</label>
+        <input
+            placeholder='Insira a sua imagem de Pefil'
+            onChange={handleInputPerfil}
+            value
+          />
+
+        <label>Url de imagem de Postagem:</label>
+        <input
+            placeholder='Insira a sua nova Postagem'
+            onChange={handleInputPostagem}
+            value
+          />
+      </Form>
+      <button>Adiciona</button> */}
+
       {listagemGeral}
   
   </MainContainer>
--- modulo3/estados-insta4/insta4/src/style.js
+++ modulo3/estados-insta4/insta4/src/style.js
@@ -69,6 +69,21 @@ export const InputComentario = styled.input`
     width: 100%;
     margin-right: 5px;
 `
-
+export const Form = styled.form`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    label {
+        position: absolute;
+        width: 1px;
+        height: 1px;
+        padding: 0;
+        margin: -1px;
+        overflow: hidden;
+        clip: rect(0,0,0,0);
+        border: 0;
+    }
+`
 
 
