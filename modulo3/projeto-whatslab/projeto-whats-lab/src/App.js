84ec2189edafb368620f5a82d0ebc58ed0f5cb64
Lucas <luucasferreiira21@gmail.com>
2022-07-11 21:19:38 -0300

    README.md

--- modulo3/projeto-whatslab/projeto-whats-lab/src/App.js
+++ modulo3/projeto-whatslab/projeto-whats-lab/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Footer, Header, Container, Colunas, Principal, Main, RoundedInput, NovaMensagem, ArrayMensagem } from "./style";
 import { createGlobalStyle } from 'styled-components';
-import { Container,Header, Main, Colunas, Principal, Footer } from './style';
+import { useState } from 'react';
 
 
 const GlobalStyle = createGlobalStyle`
@@ -13,27 +14,60 @@ const GlobalStyle = createGlobalStyle`
 `
 
 
+
 function App() {
+
+const [receberMensagem, setReceberMensagem] = useState('')
+const [receberRemetente, setReceberRemetente] = useState('')
+const [arrayInicial, setArrayInicial] = useState([])
+
+const novoArray = arrayInicial.map((elemento, index)=>{
+  return (
+    <NovaMensagem key={index}>
+      <p>{elemento.nome}</p>
+      <span>{elemento.msg}</span>
+    </NovaMensagem>
+  )
+}) 
+
+
+const addMensagem = (e) =>{
+  e.preventDefault();
+const novaMensagem = {nome:receberRemetente, msg:receberMensagem}
+
+const novoArray = [...arrayInicial, novaMensagem]
+
+setArrayInicial(novoArray)
+setReceberRemetente('')
+setReceberMensagem('')
+
+}
+
+
+
+
+
   return (
     <Container>
       <GlobalStyle/>
       <Header>
-        <img src='https://avatars.githubusercontent.com/u/53058090?s=280&v=4'/>
-        <h1>
-          WhatsLab
-        </h1>
+        <img src='https://avatars.githubusercontent.com/u/53058090?s=280&v=4' alt='imagem'/>
+        <h1>LabZap</h1>
       </Header>
       <Main>
         <Colunas/>
         <Principal>
-          <div className='rounded-input'>
+          <ArrayMensagem>
+          {novoArray}
+          </ArrayMensagem>
+          <RoundedInput>
             <label>Remetente: </label>
-                <input className='remetente' type="text" /> 
+                <input onChange={(e)=>{setReceberRemetente(e.target.value)}} value={receberRemetente}/> 
 
             <label>Msg: </label>
-                <input className='mensagem' type="text" />
-                <button>Enviar Mensagem</button>
-                </div>
+                <input onChange={(e) =>{setReceberMensagem(e.target.value)}} value={receberMensagem} />
+                <button onClick={addMensagem}>Enviar Mensagem</button>
+                </RoundedInput>
                 
         </Principal>
         <Colunas/>
