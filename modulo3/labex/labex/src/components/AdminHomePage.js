0e994dada8393f0fb217fbf4e34b9bb1ad95ef2a
Lucas <luucasferreiira21@gmail.com>
2022-08-09 23:05:16 -0300

    adminHomePage e listTrips

---
+++ modulo3/labex/labex/src/components/AdminHomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const AdminHomePage=()=>{
+    const navigate=useNavigate();
+
+    return(
+        <>
+            <h1>Login</h1>
+            <ul typeof="email">email</ul>
+            <ul typeof="senha">senha</ul>
+        </>
+    )
+}
+export default AdminHomePage;
